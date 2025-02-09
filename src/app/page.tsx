8deba7a5b6db2b0b66ffff126f63d448c2770fb2
Surya Pratap Singh <surya.singh@enertics.ca>
2025-02-09 22:39:14 +0530

    Added profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ const Page = async () => {
   const user = await currentUser();
   const posts = await getPosts();
   const dbUserId = await getDbUserId();
-  console.log(posts);
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
       <div className="lg:col-span-6">
